b83cdda79a4e998579cb9e4b042463ec0df81adc
code1218 <skjil1218@gmail.com>
2023-09-26 12:29:16 +0900

    인증 구현 중

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -4,14 +4,16 @@ import Top from '../../components/Layouts/SignInAndUpLayout/Top/Top';
 import Input from '../../components/Layouts/SignInAndUpLayout/Input/Input';
 import OrBar from '../../components/Layouts/SignInAndUpLayout/OrBar/OrBar';
 import { useNavigate } from 'react-router-dom';
+import { signin } from '../../apis/api/account';
 
 function Signin(props) {
     const navigate = useNavigate();
 
     const emptyAccount = {
-        phoneAndEmailAndUsername: "",
+        phoneOrEmailOrUsername: "",
         loginPassword: ""
     }
+
     const [ account, setAccount ] = useState(emptyAccount);
     const [ isAccountValuesEmpty, setIsAccountValuesEmpty ] = useState(true);
     const [ errorMsg, setErrorMsg ] = useState("");
@@ -27,16 +29,27 @@ function Signin(props) {
         setIsAccountValuesEmpty(Object.values(account).includes(""))
     }, [account])
 
+    const handleSigninSubmit = async () => {
+        try {
+            await signin(account);
+        } catch(error) {
+            setErrorMsg(error.response.data.errorMessage);
+        }
+    }
+
     return (
         <SignInAndUpLayout>
             <Top>
                 <div>
-                    <Input placeholder={"전화번호, 사용자이름 또는 이메일"} name={"phoneAndEmailAndUsername"} changeAccount={changeAccount}/>
+                    <Input placeholder={"전화번호, 사용자이름 또는 이메일"} name={"phoneOrEmailOrUsername"} changeAccount={changeAccount}/>
                     <Input placeholder={"비밀번호"} type={"password"} name={"loginPassword"} changeAccount={changeAccount}/>
-                    <button disabled={isAccountValuesEmpty}>로그인</button>
+                    <button onClick={handleSigninSubmit} disabled={isAccountValuesEmpty}>로그인</button>
                     <OrBar />
                     <div>
                         kakao로 로그인
+                    </div>
+                    <div>
+                        {errorMsg}
                     </div>
                 </div>
             </Top>
